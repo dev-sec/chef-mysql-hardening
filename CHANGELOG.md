0f7b9befdad4fe729f4f0770bac232ad79adf323
Christoph Hartmann <chris@lollyrock.com>
2014-07-29 16:27:00 +0200

    bump version to 1.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## 1.0.0
+
+* feature: work in conjunction with mysql cookbook
+* feature: add hardening configuration in separate file
+* feature: test against serverspec
+* feature: implement various hardening parameters for mysql
--- metadata.rb
+++ metadata.rb
@@ -21,7 +21,7 @@ maintainer_email "chris@lollyrock.com"
 license          "Apache v2.0"
 description      "Installs and configures a secure mysql server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "1.0.0"
 
 depends 'mysql' , "> 5.2.0"
 
